5637762fc1177e212981977e63cd5823aad34181
TheAssassin <theassassin@users.noreply.github.com>
2016-05-30 22:38:23 +0200

    Sort players by name in frontend

--- frontend/redflare.js
+++ frontend/redflare.js
@@ -8,6 +8,9 @@ redflareApp.controller("ServerTableCtrl", function($scope, $http, $interval) {
             for (var i = 0; i < servers.length; i++) {
                 var server = servers[i]
                 server.mutators = server.mutators.join("-")
+                server.players.sort(function(a, b) {
+                    return a.name.toLowerCase() > b.name.toLowerCase()
+                })
             }
             $scope.servers = servers
             $scope.loading = 0
