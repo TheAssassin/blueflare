9a4ca58cd5931351f9be6fdb0da73a7c4571f572
TheAssassin <theassassin@users.noreply.github.com>
2016-05-30 14:08:18 +0200

    Implement a 9 seconds cache for master server replies

--- redflare/master_client.py
+++ redflare/master_client.py
@@ -1,30 +1,57 @@
 import socket
-from tornado import gen, iostream
+import time
+from tornado import gen, iostream, locks
 from .server import Server
 
 
 class MasterClient:
+    _cache = None
+    _lock = locks.Lock()
+
     def __init__(self, hostname, port):
         self.hostname = hostname
         self.port = port
 
     @gen.coroutine
     def fetch_servers(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        stream = iostream.IOStream(sock)
-        servers = []
+        def try_cache():
+            cache = self.__class__._cache
+            # wait 9 seconds before bothering the master server again
+            if cache is not None and time.time() - cache["timestamp"] < 9:
+                # use exception to return the data to abort the execution of
+                # the coroutine without any return statements
+                raise gen.Return(cache["data"])
 
-        stream.connect((self.hostname, self.port))
+        # return cached value if possible, otherwise refresh the cache
+        try_cache()
 
-        yield stream.write(b"update\n")
-        data = yield stream.read_until_close()
+        with (yield self.__class__._lock.acquire()):
+            # second try, the cache might have been filled in the meanwhile
+            try_cache()
 
-        lines = data.decode().splitlines()
+            # otherwise query the server again
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            stream = iostream.IOStream(sock)
+            servers = []
 
-        for line in lines:
-            if line.startswith("addserver"):
-                server = Server.from_addserver_line(line)
-                servers.append(server)
+            stream.connect((self.hostname, self.port))
 
-        stream.close()
-        return servers
+            yield stream.write(b"update\n")
+            data = yield stream.read_until_close()
+
+            lines = data.decode().splitlines()
+
+            for line in lines:
+                if line.startswith("addserver"):
+                    server = Server.from_addserver_line(line)
+                    servers.append(server)
+
+            stream.close()
+
+            # fill the cache
+            self.__class__._cache = {
+                "timestamp": time.time(),
+                "data": servers,
+            }
+
+            return servers
