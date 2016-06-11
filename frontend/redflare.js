c9fa9ee4cfb7500ffb5d861b95f89b3479146ef6
TheAssassin <theassassin@users.noreply.github.com>
2016-06-11 17:02:27 +0200

    Display remaining time in mm:ss format (fixes #3)

--- frontend/redflare.js
+++ frontend/redflare.js
@@ -5,17 +5,30 @@ redflareApp.controller("ServerTableCtrl", function($scope, $http, $interval) {
         return "privilege-icon/" + player.privilege + "/" + player.color.slice(1, 7) + ".svg"
     }
 
+    var formatTime = function(seconds) {
+        if (seconds < 0) {
+            return "∞"
+        } else {
+            var remainingMinutes = Math.floor(seconds / 60)
+            var remainingSeconds = Math.floor(seconds % 60)
+
+            return remainingMinutes + ":" + ("00" + remainingSeconds).slice(-2)
+        }
+    }
+
     var fetch = function() {
         $scope.loading = 1
         $http.get("api/servers.json").then(function(response) {
             var servers = response.data.servers
             servers.forEach(function(server) {
                 server.mutators = server.mutators.join("-")
+                server.time_formatted = formatTime(server.time_remaining)
             })
             $scope.servers = servers
             $scope.loading = 0
         })
     }
+
     $interval(fetch, 10000)
     fetch()
 })
